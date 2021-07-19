d6300f765a4b45e23eb4eb716ff223741c093ca1
Shivaansh1808 <72135426+Shivaansh1808@users.noreply.github.com>
2021-07-19 22:39:23 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class CreatePost extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>
+                    Create Post
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+  });
---
+++ screens/Feed.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Feed extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>
+                    Feeds
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+        
+  });
